a65bd2318e770e08e674a168fec2b024b9fe7e97
EskildVeren <eskild.overen@gmail.com>
2025-04-02 21:45:15 +0200

    feat: :sparkles: add page for authentication

--- app/src/pages/authenticationPage/AuthenticationPage.tsx
+++ app/src/pages/authenticationPage/AuthenticationPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AuthenticationPage() {
-  return <div>AuthenticationPage</div>;
+  return (
+    <>
+      <Link to={"jobs"}>To job page</Link>
+      <div className="">AuthenticationPage</div>
+      <p>You will authenticate!</p>
+      <div>
+        <p>Yes.</p>
+      </div>
+    </>
+  );
 }
 
 export default AuthenticationPage;
