0e5b563079d47db500997ccbf803992c3dfda59b
EskildVeren <eskild.overen@gmail.com>
2025-04-12 13:40:37 +0200

    feat: :fire: remove mockjobs

--- app/src/lib/jobData.ts
+++ app/src/lib/jobData.ts
@@ -6,50 +6,6 @@ export type JobAdvert = {
   priority: string;
   hyperlink: string;
   advertisementSite: string;
+  city: string;
   appliedFor: boolean;
 };
-
-export function getJobs(): JobAdvert[] {
-  return [
-    {
-      advert_id: "0",
-      companyName: "Variant",
-      positionName: "Webutvikler",
-      priority: "Høy",
-      deadline: "4. mars",
-      hyperlink: "https://jobs.variant.no/o/utvikler-i-oslo",
-      advertisementSite: "Variant",
-      appliedFor: false,
-    },
-    {
-      advert_id: "1",
-      companyName: "Bekk",
-      positionName: "Frontend utvikler",
-      priority: "Medium",
-      deadline: "4. mars",
-      hyperlink: "https://jobs.variant.no/o/utvikler-i-oslo",
-      advertisementSite: "Finn",
-      appliedFor: true,
-    },
-    {
-      advert_id: "2",
-      companyName: "Kantega",
-      positionName: "UX",
-      priority: "Høy",
-      deadline: "4. mars",
-      hyperlink: "https://jobs.variant.no/o/utvikler-i-oslo",
-      advertisementSite: "Finn",
-      appliedFor: false,
-    },
-    {
-      advert_id: "3",
-      companyName: "NAV",
-      positionName: "Backend utvikler",
-      priority: "Høy",
-      deadline: "4. mars",
-      hyperlink: "https://jobs.variant.no/o/utvikler-i-oslo",
-      advertisementSite: "Abakus",
-      appliedFor: false,
-    },
-  ];
-}
