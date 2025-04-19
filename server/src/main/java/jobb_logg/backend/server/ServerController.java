a7753054a4bcae89577929be903664cff700cc43
EskildVeren <eskild.overen@gmail.com>
2025-04-19 23:46:42 +0200

    style: :truck: rename GrettingsController to ServerController

--- server/src/main/java/jobb_logg/backend/server/ServerController.java
+++ server/src/main/java/jobb_logg/backend/server/ServerController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class ServerController {
 
     @CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*")
     @PostMapping(value = "/jobAdverts", consumes = { "application/json" })
